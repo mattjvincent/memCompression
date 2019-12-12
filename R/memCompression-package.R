73234321bbcd456af9bf005ddfd457741b019225
Matt Vincent <matt.vincent@jax.org>
2019-12-12 09:25:59 -0500

    Initial import

---
+++ R/memCompression-package.R
@@ -0,0 +1,11 @@
+#' Making memCompress better
+#'
+#' A partial resurrection of `gzmem` for
+#' \href{http://stackoverflow.com/q/39707388/1457051}{this}.
+#'
+#' @name memCompression
+#' @docType package
+#' @author Matthew Vincent
+#' @useDynLib memCompression
+#' @importFrom Rcpp sourceCpp
+NULL
